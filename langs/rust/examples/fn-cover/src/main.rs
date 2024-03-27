86c74b446a255918e9848aa147929a047a4579c1
Manuel Hatzl <manuel.hatzl@infineon.com>
2024-03-27 16:37:51 +0100

    feat: add feature flags for various log options

---
+++ langs/rust/examples/fn-cover/src/main.rs
@@ -0,0 +1,27 @@
+use mantra_rust_macros::req;
+
+use std::io::Write;
+
+#[req(123, 321)]
+#[req(my_req.test)]
+fn attrb_macro_usage() {
+    println!("fn body");
+}
+
+pub fn main() {
+    env_logger::builder()
+        .filter_level(mantra_rust_macros::log::LevelFilter::Trace)
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{} {} ('{}', '{}')",
+                record.level(),
+                record.args(),
+                record.file().unwrap_or("undefined"),
+                record.line().unwrap_or_default()
+            )
+        })
+        .init();
+
+    attrb_macro_usage();
+}
