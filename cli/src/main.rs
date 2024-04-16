9dedc48f1d43c600417babf96e11bc5a4e994458
Manuel Hatzl <49341624+mhatzl@users.noreply.github.com>
2024-04-16 14:53:13 +0200

    feat: add req extraction and db setup

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use mantra_db::MantraDb;
+
+#[tokio::main]
+async fn main() {
+    let db_cfg = mantra_db::Config {
+        url: Some("sqlite:mantra.db".to_string()),
+    };
+    let db = MantraDb::new(&db_cfg).await.unwrap();
+
+    let extract_cfg = mantra_extract::Config {
+        root: PathBuf::from(r"../mantra-wiki/5-Requirements"),
+        link: "test".to_string(),
+        origin: mantra_extract::ExtractOrigin::GitHub,
+    };
+    mantra_extract::extract(&db, &extract_cfg).await.unwrap();
+}
+
+pub struct Config {
+    pub db: ConfigDb,
+    pub trace: ConfigTrace,
+    pub extract: ConfigExtract,
+}
+
+pub struct ConfigDb {
+    /// URL to connect to a SQL database.
+    /// Default is a SQLite file named `mantra.db` that is located under `.mantra/` at the workspace root.
+    pub url: Option<String>,
+}
+
+pub struct ConfigTrace {
+    pub project_name: String,
+    pub root_dir: PathBuf,
+}
+
+pub struct ConfigExtract {
+    pub root_dir: PathBuf,
+    pub origin_kind: ExtractOriginKind,
+}
+
+pub enum ExtractOriginKind {
+    GitHub,
+    Jira,
 }
--- extract/src/lib.rs
+++ extract/src/lib.rs
@@ -1,14 +1,127 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::path::{Path, PathBuf};
+
+use ignore::{types::TypesBuilder, WalkBuilder};
+use mantra_db::{GitHubReqOrigin, MantraDb, Requirement};
+use regex::Regex;
+
+pub async fn extract(db: &MantraDb, cfg: &Config) -> Result<(), ExtractError> {
+    match cfg.origin {
+        ExtractOrigin::GitHub => extract_github(db, &cfg.root, &cfg.link).await,
+        ExtractOrigin::Jira => todo!(),
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+async fn extract_github(db: &MantraDb, root: &Path, link: &str) -> Result<(), ExtractError> {
+    if root.is_dir() {
+        let walk = WalkBuilder::new(root)
+            .types(
+                TypesBuilder::new()
+                    .add_defaults()
+                    .select("markdown")
+                    .build()
+                    .expect("Could not create markdown file filter."),
+            )
+            .build();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        for dir_entry_res in walk {
+            let dir_entry = match dir_entry_res {
+                Ok(entry) => entry,
+                Err(_) => continue,
+            };
+
+            if dir_entry
+                .file_type()
+                .expect("No file type found for given entry. Note: stdin is not supported.")
+                .is_file()
+            {
+                let filepath = dir_entry.path().to_string_lossy().to_string();
+                let content = std::fs::read_to_string(dir_entry.path())
+                    .map_err(|_| ExtractError::CouldNotAccessFile(filepath))?;
+
+                let reqs = extract_from_wiki_content(&content, dir_entry.path(), link);
+
+                if reqs.is_empty() {
+                    // warn that no reqs were found
+                    continue;
+                }
+
+                db.add_reqs(reqs).await.map_err(ExtractError::DbError)?
+            }
+        }
+    } else {
+        let filepath = root.to_string_lossy().to_string();
+        let content = std::fs::read_to_string(root)
+            .map_err(|_| ExtractError::CouldNotAccessFile(filepath))?;
+
+        let reqs = extract_from_wiki_content(&content, root, link);
+
+        if reqs.is_empty() {
+            // warn that no reqs were found
+        } else {
+            db.add_reqs(reqs).await.map_err(ExtractError::DbError)?
+        }
     }
+
+    Ok(())
+}
+
+static REQ_ID_MATCHER: std::sync::OnceLock<Regex> = std::sync::OnceLock::new();
+
+fn extract_from_wiki_content(content: &str, filepath: &Path, link: &str) -> Vec<Requirement> {
+    let lines = content.lines();
+
+    let mut reqs = Vec::new();
+    let mut in_verbatim_context = false;
+
+    let regex = REQ_ID_MATCHER.get_or_init(|| {
+        Regex::new(r"^#+\s`(?<id>[^\s:]+)`:\s.+")
+            .expect("Regex to match the requirement ID could **not** be created.")
+    });
+
+    for (line_nr, line) in lines.enumerate() {
+        if line.trim_start().starts_with("```") || line.trim_start().starts_with("~~~") {
+            in_verbatim_context = !in_verbatim_context;
+        }
+
+        if !in_verbatim_context {
+            if let Some(captures) = regex.captures(line) {
+                let id = captures
+                    .name("id")
+                    .expect("`id` capture group was not in heading match.")
+                    .as_str()
+                    .to_string();
+
+                reqs.push(Requirement {
+                    id,
+                    origin: mantra_db::RequirementOrigin::GitHub(GitHubReqOrigin {
+                        root: link.to_string(),
+                        path: filepath.to_path_buf(),
+                        line: line_nr,
+                    })
+                    .into(),
+                });
+            }
+        }
+    }
+
+    reqs
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub root: PathBuf,
+    pub link: String,
+    pub origin: ExtractOrigin,
+}
+
+#[derive(Debug)]
+pub enum ExtractOrigin {
+    GitHub,
+    Jira,
+}
+
+#[derive(Debug)]
+pub enum ExtractError {
+    CouldNotAccessFile(String),
+    DbError(mantra_db::DbError),
 }
