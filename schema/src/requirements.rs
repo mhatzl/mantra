867ec21240c900a893cbea83c8306bc4343f193e
Manuel Hatzl <49341624+mhatzl@users.noreply.github.com>
2024-10-19 19:50:07 +0200

    fix: add documentation

--- schema/src/requirements.rs
+++ schema/src/requirements.rs
@@ -3,16 +3,23 @@ pub struct RequirementSchema {
     pub requirements: Vec<Requirement>,
 }
 
+/// Type alias for a requirement ID
 pub type ReqId = String;
 
 #[derive(
     Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, schemars::JsonSchema,
 )]
 pub struct Requirement {
+    /// ID of the requirement
     pub id: ReqId,
+    /// Title of the requirement
     pub title: String,
+    /// Link to the origin the requirement is defined
     pub link: String,
+    /// true: Marks the requirement to require manual verification
     pub manual: bool,
+    /// true: Marks the requirement to be deprecated
     pub deprecated: bool,
+    /// Field to store custom information per requirement
     pub info: Option<serde_json::Value>,
 }
