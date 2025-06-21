8998819e6c377f97a75387a6b9c31f34fbd0e176
Manuel Hatzl <hatzlmanuel@outlook.com>
2025-06-22 00:02:11 +0200

    feat: update req schema to new db schema

--- schema/src/requirements.rs
+++ schema/src/requirements.rs
@@ -1,3 +1,7 @@
+use std::path::PathBuf;
+
+/// Defines the schema to exchange requirements related information.
+/// [req("exchange.requirements")]
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize, schemars::JsonSchema)]
 pub struct RequirementSchema {
     #[serde(serialize_with = "crate::serialize_schema_version")]
@@ -6,29 +10,84 @@ pub struct RequirementSchema {
 }
 
 /// Type alias for a requirement ID.
+/// [req("req.id")]
 pub type ReqId = String;
 
+/// This struct defines the information *mantra* stores about a requirement.
+/// [req("req")]
 #[derive(
     Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, schemars::JsonSchema,
 )]
+#[serde(rename_all = "kebab-case")]
 pub struct Requirement {
     /// ID of the requirement.
+    /// [req("req.id")]
     pub id: ReqId,
     /// Hash of the requirement content to detect changes.
     ///
-    /// If not provided, will be computed using the fields: parents, title, origin, manual, deprecated, data
-    #[serde(alias = "content-hash")]
+    /// If not provided, will be computed using the fields:
+    /// parents, title, origin, manual_verification, deprecated, properties
     pub content_hash: Option<String>,
     /// Optional list of parent requirement IDs.
+    /// [req("req.hierarchy.mult_parents")]
     pub parents: Option<Vec<ReqId>>,
     /// Title of the requirement.
+    /// [req("req.title")]
     pub title: String,
-    /// Link to the origin the requirement is defined.
-    pub origin: String,
+    /// Optional description of the requirement.
+    /// [req("req.description")]
+    pub description: Option<String>,
+    /// Origin where the requirement is defined at.
+    /// [req("req.origin")]
+    pub origin: RequirementOrigin,
     /// true: Marks the requirement to require manual verification.
-    pub manual: bool,
+    /// [req("req.manual")]
+    pub manual_verification: bool,
     /// true: Marks the requirement to be deprecated.
+    /// [req("req.deprecated")]
     pub deprecated: bool,
-    /// Field to store custom information per requirement.
-    pub data: Option<serde_json::Value>,
+    /// Field to store custom properties per requirement.
+    /// [req("req.properties")]
+    pub properties: Option<serde_json::Value>,
+}
+
+/// Defines the origin variants of a requirement.
+/// [req("req.origin")]
+#[derive(
+    Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, schemars::JsonSchema,
+)]
+pub enum RequirementOrigin {
+    /// Marks that a requirement was defined in a markup-based Wiki.
+    /// [req("req.origin.wiki")]
+    Wiki(WikiRequirementOrigin),
+    /// Marks that a requirement was defined in an external source.
+    /// [req("req.origin.extern")]
+    Extern(ExternRequirementOrigin),
+}
+
+/// Struct for the wiki origin of a requirement.
+/// [req("req.origin.wiki")]
+#[derive(
+    Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, schemars::JsonSchema,
+)]
+#[serde(rename_all = "kebab-case")]
+pub struct WikiRequirementOrigin {
+    /// The file the requirement is defined in in the wiki.
+    pub filepath: PathBuf,
+    /// The line the requirement is defined at.
+    pub line: u32,
+    /// Optional URL to the repository of the wiki.
+    pub repo_url: Option<String>,
+    /// Optional URL to the rendered view of the wiki.
+    pub rendered_url: Option<String>,
+}
+
+/// Struct for the external origin of a requirement.
+/// [req("req.origin.extern")]
+#[derive(
+    Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize, schemars::JsonSchema,
+)]
+pub struct ExternRequirementOrigin {
+    /// The URL a requirement is defined at externally to mantra.
+    pub url: String,
 }
