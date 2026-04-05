f3eb499e20731b15a81dcd2b52fa97a2d4a606ac
Manuel Hatzl <hatzlmanuel@outlook.com>
2026-04-05 13:34:39 +0200

    Add req state tests

--- mantra/src/cmd/collect/requirements/tests/mod.rs
+++ mantra/src/cmd/collect/requirements/tests/mod.rs
@@ -11,3 +11,343 @@ async fn detect_hierarchy_cycles(dir: &str) {
     };
     assert_eq!(db_err.to_string(), "Requirement cycle detected!");
 }
+
+mod states {
+    use mantra_schema::report::short::RequirementState;
+
+    use crate::cmd::collect::test_setup::db_from_dir;
+
+    #[tokio::test]
+    async fn deprecated_req() {
+        let depr_state = RequirementState::Deprecated.as_nr();
+
+        let mut db = db_from_dir!("states/deprecated").unwrap();
+
+        let depr_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            depr_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            depr_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to have deprecated state."
+        );
+        assert!(
+            depr_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to have deprecated state indirectly due to req-1."
+        );
+        assert!(
+            !depr_reqs.contains(&"req-2".to_string()),
+            "Req-2 must not be marked as deprecated."
+        );
+        assert!(
+            depr_reqs.contains(&"req-2.sub-1".to_string()),
+            "Expected req-2.sub-1 to have deprecated state."
+        );
+        assert!(
+            depr_reqs.contains(&"req-3".to_string()),
+            "Expected req-3 to have deprecated state."
+        );
+        assert!(
+            depr_reqs.contains(&"req-4".to_string()),
+            "Expected req-4 to have deprecated state, even if covering test failed."
+        );
+        assert!(
+            depr_reqs.contains(&"req-5".to_string()),
+            "Expected req-5 to have deprecated state, even if covered test was skipped."
+        );
+        assert!(
+            depr_reqs.contains(&"req-6".to_string()),
+            "Expected req-6 to have deprecated state, even if covered test passed."
+        );
+        assert!(
+            depr_reqs.contains(&"req-7".to_string()),
+            "Expected req-7 to have deprecated state, even if req was manually verified."
+        );
+        assert!(
+            depr_reqs.contains(&"req-8".to_string()),
+            "Expected req-8 to have deprecated state."
+        );
+        assert!(
+            depr_reqs.contains(&"req-9".to_string()),
+            "Expected req-9 to have deprecated state."
+        );
+    }
+
+    #[tokio::test]
+    async fn ignored_req() {
+        let ignore_state = RequirementState::Ignored.as_nr();
+
+        let mut db = db_from_dir!("states/ignored").unwrap();
+
+        let ignored_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            ignore_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            ignored_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to have ignored state."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to have ignored state indirectly due to req-1."
+        );
+        assert!(
+            !ignored_reqs.contains(&"req-2".to_string()),
+            "Req-2 must not be marked as ignored."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-2.sub-1".to_string()),
+            "Expected req-2.sub-1 to have ignored state."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-3".to_string()),
+            "Expected req-3 to have ignored state."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-4".to_string()),
+            "Expected req-4 to have ignored state, even if covering test failed."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-5".to_string()),
+            "Expected req-5 to have ignored state, even if covered test was skipped."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-6".to_string()),
+            "Expected req-6 to have ignored state, even if covered test passed."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-7".to_string()),
+            "Expected req-7 to have ignored state, even if req was manually verified."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-8".to_string()),
+            "Expected req-8 to have ignored state."
+        );
+        assert!(
+            ignored_reqs.contains(&"req-9".to_string()),
+            "Expected req-9 to have ignored state."
+        );
+    }
+
+    #[tokio::test]
+    async fn failed_req() {
+        let failed_state = RequirementState::Failed.as_nr();
+
+        let mut db = db_from_dir!("states/failed").unwrap();
+
+        let failed_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            failed_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            failed_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to have failed state."
+        );
+        assert!(
+            !failed_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to not be failed."
+        );
+        assert!(
+            failed_reqs.contains(&"req-2".to_string()),
+            "Expected req-2 to be failed indirectly due to req-2.sub-1."
+        );
+        assert!(
+            failed_reqs.contains(&"req-2.sub-1".to_string()),
+            "Expected req-2.sub-1 to have failed state."
+        );
+        assert!(
+            failed_reqs.contains(&"req-skipped".to_string()),
+            "Expected req-skipped to have failed state, even if other covered test was skipped."
+        );
+        assert!(
+            failed_reqs.contains(&"req-verified".to_string()),
+            "Expected req-verified to have failed state, even if other covered test passed."
+        );
+        assert!(
+            failed_reqs.contains(&"req-manual".to_string()),
+            "Expected req-manual to have failed state, even if req was manually verified."
+        );
+    }
+
+    #[tokio::test]
+    async fn skipped_req() {
+        let skipped_state = RequirementState::Skipped.as_nr();
+
+        let mut db = db_from_dir!("states/skipped").unwrap();
+
+        let skipped_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            skipped_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            skipped_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to have skipped state."
+        );
+        assert!(
+            !skipped_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to not be skipped."
+        );
+        assert!(
+            skipped_reqs.contains(&"req-2".to_string()),
+            "Expected req-2 to be skipped indirectly due to req-2.sub-1."
+        );
+        assert!(
+            skipped_reqs.contains(&"req-2.sub-1".to_string()),
+            "Expected req-2.sub-1 to have skipped state."
+        );
+        assert!(
+            skipped_reqs.contains(&"req-verified".to_string()),
+            "Expected req-verified to have skipped state, even if other covered test passed."
+        );
+        assert!(
+            skipped_reqs.contains(&"req-manual".to_string()),
+            "Expected req-manual to have skipped state, even if req was manually verified."
+        );
+    }
+
+    #[tokio::test]
+    async fn unverified_req() {
+        let unverified_state = RequirementState::Unverified.as_nr();
+
+        let mut db = db_from_dir!("states/unverified").unwrap();
+
+        let unverified_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            unverified_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            !unverified_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to have verified state."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to remain unverified, even if parent is verified."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-2".to_string()),
+            "Expected req-2 to have unverified state."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-2.sub-1".to_string()),
+            "Expected req-2.sub-1 to have unverified state."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-manual".to_string()),
+            "Expected req-manual to have unverified state, even if req was verified by test, because manual review is missing."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-satisfy".to_string()),
+            "Expected req-satisfy to have unverified state, because no test covers the trace."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-verify".to_string()),
+            "Expected req-verify to have unverified state, because no test covers the trace."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-satisfy-verify".to_string()),
+            "Expected req-satisfy-verify to have unverified state,
+            because the test covering the verify trace does not cover the satisfy trace and vice versa."
+        );
+        assert!(
+            unverified_reqs.contains(&"req-mult-satisfy".to_string()),
+            "Expected req-mult-satisfy to have unverified state,
+            because not all satisfy traces are covered by tests."
+        );
+    }
+
+    #[tokio::test]
+    async fn verified_req() {
+        let verified_state = RequirementState::Verified.as_nr();
+
+        let mut db = db_from_dir!("states/verified").unwrap();
+
+        let verified_reqs: Vec<String> = sqlx::query!(
+            "
+            select id from RequirementVerificationStates
+            where state = $1
+            ",
+            verified_state
+        )
+        .fetch_all(db.connection_mut())
+        .await
+        .unwrap()
+        .into_iter()
+        .map(|r| r.id)
+        .collect();
+
+        assert!(
+            verified_reqs.contains(&"req-1".to_string()),
+            "Expected req-1 to be indirectly verified through req-1.sub-1."
+        );
+        assert!(
+            verified_reqs.contains(&"req-1.sub-1".to_string()),
+            "Expected req-1.sub-1 to have verified state."
+        );
+        assert!(
+            verified_reqs.contains(&"req-manual".to_string()),
+            "Expected req-manual to have verified state."
+        );
+        assert!(
+            verified_reqs.contains(&"req-satisfy".to_string()),
+            "Expected req-satisfy to have verified state."
+        );
+        assert!(
+            verified_reqs.contains(&"req-verify".to_string()),
+            "Expected req-verify to have verified state."
+        );
+        assert!(
+            verified_reqs.contains(&"req-satisfy-verify".to_string()),
+            "Expected req-satisfy-verify to have verified state"
+        );
+    }
+}
