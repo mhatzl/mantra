82db280d4337b60fb6d1b5085ee589a869cb1b79
Manuel Hatzl <hatzlmanuel@outlook.com>
2026-06-23 17:25:38 +0200

    Set start line for annotation collection to 1
    IDEs typically start line counting at 1

--- langs/mantra-lang-tracing/src/collect/collector.rs
+++ langs/mantra-lang-tracing/src/collect/collector.rs
@@ -2,7 +2,7 @@ use mantra_schema::{Line, annotations::Annotations};
 
 pub trait AnnotationCollector {
     fn collect(content: &str) -> Result<Annotations, anyhow::Error> {
-        Self::collect_relative(content, 0)
+        Self::collect_relative(content, 1)
     }
 
     fn collect_relative(content: &str, start_line: Line) -> Result<Annotations, anyhow::Error>;
